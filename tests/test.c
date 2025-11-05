a732c750f2ccacc8bfce0a33699ebec2032cf090
robo-monk <rrobomonk@gmail.com>
2025-11-06 00:11:19 +0100

    skip

--- tests/test.c
+++ tests/test.c
@@ -16,54 +16,76 @@ static inline double ms_since(struct timespec start) {
          (end.tv_nsec - start.tv_nsec) / 1e6;
 }
 
-static const char *test_files[] = {
-    "tests/simple_comptype",
+// static const char *test_files[] = {
+//     "tests/simple_comptype",
 
-    "tests/simple_inline_comptime",   "tests/auto_header_injection",
+//     "tests/simple_inline_comptime",
+//     "tests/auto_header_injection",
 
-    "tests/top_level_comptime_block", "tests/comptime_type",
-    "tests/macro_repeated_arg",
-};
+//     "tests/top_level_comptime_block",
+//     "tests/comptime_type",
+//     "tests/macro_repeated_arg",
+// };
 
-static const size_t test_files_len = sizeof(test_files) / sizeof(test_files[0]);
-
+// static const size_t test_files_len = sizeof(test_files) /
+// sizeof(test_files[0]);
 int main(int argc, char **argv) {
   struct timespec t0;
   clock_gettime(CLOCK_MONOTONIC, &t0);
   nob_minimal_log_level = NOB_INFO;
-  NOB_GO_REBUILD_URSELF_PLUS(argc, argv,
-                             //
-                             "tests/test.h"
-                             //
-  );
+  NOB_GO_REBUILD_URSELF_PLUS(argc, argv, "tests/test.h");
+
+  Nob_File_Paths tests = {0};
+  nob_read_entire_dir("tests", &tests);
+
+  for (ssize_t i = tests.count-1; i >= 0; i--) {
+    if (strcmp(tests.items[i], ".") == 0 || strcmp(tests.items[i], "..") == 0 ||
+        strcmp(tests.items[i], "test.h") == 0 ||
+        strcmp(tests.items[i], "test.c") == 0) {
+      nob_da_remove_unordered(&tests, i);
+      continue;
+    }
+
+    if (strstr(tests.items[i], "_skip") != NULL) {
+      nob_log(WARNING, YELLOW("Skipping test %s"), tests.items[i]);
+      nob_da_remove_unordered(&tests, i);
+      continue;
+    }
+  }
+
+  nob_log(INFO, "Found %zu tests", tests.count);
 
   Nob_Procs procs = {0};
   nob_log(INFO, "Compiling tests...");
-  for (size_t i = 0; i < test_files_len; i++) {
+
+  // first pass: compile the tests
+  nob_da_foreach(const char *, test_file, &tests) {
+
+    nob_log(INFO, "Compiling test %s", *test_file);
     size_t mark = nob_temp_save();
 
     Nob_Cmd compile_cmd = {0};
     nob_cmd_append(&compile_cmd, "./build/ccomptime");
     nob_cmd_append(&compile_cmd, "clang");
     nob_cmd_append(&compile_cmd,
-                   nob_temp_sprintf("%s/%s", test_files[i], "main.c"));
+                   nob_temp_sprintf("tests/%s/%s", *test_file, "main.c"));
 
     nob_cmd_append(&compile_cmd, "-o",
-                   nob_temp_sprintf("%s/%s", test_files[i], "out"));
+                   nob_temp_sprintf("tests/%s/%s", *test_file, "out"));
 
     nob_cmd_run(&compile_cmd, .async = &procs,
-                .stdout_path =
-                    nob_temp_sprintf("%s/%s", test_files[i], "comp-stdout.txt"),
-                .stderr_path = nob_temp_sprintf("%s/%s", test_files[i],
+                .stdout_path = nob_temp_sprintf("tests/%s/%s", *test_file,
+                                                "comp-stdout.txt"),
+                .stderr_path = nob_temp_sprintf("tests/%s/%s", *test_file,
                                                 "comp-stderr.txt"));
 
     const char *assertion_path =
-        nob_temp_sprintf("%s/%s", test_files[i], "assertion.c");
+        nob_temp_sprintf("tests/%s/%s", *test_file, "assertion.c");
 
     if (!nob_file_exists(assertion_path)) {
       nob_log(ERROR,
               "Please include a `assertion.c` in the test. Not found for %s",
-              test_files[i]);
+              *test_file);
       continue;
     }
 
@@ -72,7 +94,7 @@ int main(int argc, char **argv) {
     nob_cmd_append(&assert_compile_cmd, "-O1");
     nob_cmd_append(&assert_compile_cmd, assertion_path);
     nob_cmd_append(&assert_compile_cmd, "-o",
-                   nob_temp_sprintf("%s/%s", test_files[i], "assertion"));
+                   nob_temp_sprintf("tests/%s/%s", *test_file, "assertion"));
     nob_cmd_run(&assert_compile_cmd, .async = &procs);
 
     nob_temp_rewind(mark);
@@ -83,32 +105,34 @@ int main(int argc, char **argv) {
     TestResult *items;
   } assertions = {0};
 
-  int comp_success = 0;
-  for (size_t i = 0; i < procs.count; ++i) {
-    if (!nob_proc_wait(procs.items[i])) {
+  nob_da_foreach(Nob_Proc, it, &procs) {
+    if (!nob_proc_wait(*it)) {
       TestResult t = (TestResult){.error = "failed to compile", .success = 0};
       nob_da_append(&assertions, t);
     }
   }
   procs.count = 0;
-  // nob_procs_flush(&procs);
 
   nob_log(INFO, "Running tests...");
-  for (size_t i = 0; i < test_files_len; i++) {
+
+  // second pass: run the tests
+  nob_da_foreach(const char *, test_file, &tests) {
+
     size_t mark = nob_temp_save();
     Nob_Cmd exe_cmd = {0};
-    nob_cmd_append(&exe_cmd, nob_temp_sprintf("%s/%s", test_files[i], "out"));
+    nob_cmd_append(&exe_cmd,
+                   nob_temp_sprintf("tests/%s/%s", *test_file, "out"));
     nob_cmd_run(&exe_cmd, .async = &procs,
-                .stdout_path =
-                    nob_temp_sprintf("%s/%s", test_files[i], "exec-stdout.txt"),
-                .stderr_path = nob_temp_sprintf("%s/%s", test_files[i],
+                .stdout_path = nob_temp_sprintf("tests/%s/%s", *test_file,
+                                                "exec-stdout.txt"),
+                .stderr_path = nob_temp_sprintf("tests/%s/%s", *test_file,
                                                 "exec-stderr.txt"));
 
     nob_temp_rewind(mark);
   }
 
-  for (size_t i = 0; i < procs.count; ++i) {
-    if (!nob_proc_wait(procs.items[i])) {
+  nob_da_foreach(Nob_Proc, it, &procs) {
+    if (!nob_proc_wait(*it)) {
       TestResult t = (TestResult){.error = "failed to run test", .success = 0};
       nob_da_append(&assertions, t);
     }
@@ -116,14 +140,14 @@ int main(int argc, char **argv) {
   procs.count = 0;
 
   nob_log(INFO, "Running assertions...");
-  int passed = 0;
-  int failed = 0;
 
-  for (size_t i = 0; i < test_files_len; i++) {
+  // third pass: run the assertions
+  nob_da_foreach(const char *, test_file, &tests) {
+
     size_t mark = nob_temp_save();
     Nob_Cmd assert_run_cmd = {0};
     nob_cmd_append(&assert_run_cmd,
-                   nob_temp_sprintf("%s/%s", test_files[i], "assertion"));
+                   nob_temp_sprintf("tests/%s/%s", *test_file, "assertion"));
 
     int result = nob_cmd_run(&assert_run_cmd, .async = &procs);
     if (!result) {
@@ -144,10 +168,12 @@ int main(int argc, char **argv) {
   procs.count = 0;
 
   // parse results
-  for (size_t i = 0; i < test_files_len; i++) {
+  // fourth pass: parse the assertion results
+  nob_da_foreach(const char *, test_file, &tests) {
+
     Nob_String_Builder results = {0};
     nob_read_entire_file(
-        nob_temp_sprintf("%s/%s", test_files[i], "assertion-results.txt"),
+        nob_temp_sprintf("tests/%s/%s", *test_file, "assertion-results.txt"),
         &results);
 
     TestResults tr = {0};
@@ -156,25 +182,29 @@ int main(int argc, char **argv) {
   }
 
   // cleanup
-  for (size_t i = 0; i < test_files_len; i++) {
+  nob_da_foreach(const char *, test_file, &tests) {
+
     size_t mark = nob_temp_save();
-    nob_delete_file(nob_temp_sprintf("%s/%s", test_files[i], "assertion"));
+    nob_delete_file(nob_temp_sprintf("tests/%s/%s", *test_file, "assertion"));
     nob_delete_file(
-        nob_temp_sprintf("%s/%s", test_files[i], "assertion-results.txt"));
+        nob_temp_sprintf("tests/%s/%s", *test_file, "assertion-results.txt"));
     nob_delete_file(
-        nob_temp_sprintf("%s/%s", test_files[i], "exec-stdout.txt"));
+        nob_temp_sprintf("tests/%s/%s", *test_file, "exec-stdout.txt"));
     nob_delete_file(
-        nob_temp_sprintf("%s/%s", test_files[i], "exec-stderr.txt"));
+        nob_temp_sprintf("tests/%s/%s", *test_file, "exec-stderr.txt"));
     nob_delete_file(
-        nob_temp_sprintf("%s/%s", test_files[i], "comp-stderr.txt"));
+        nob_temp_sprintf("tests/%s/%s", *test_file, "comp-stderr.txt"));
     nob_delete_file(
-        nob_temp_sprintf("%s/%s", test_files[i], "comp-stdout.txt"));
-    nob_delete_file(nob_temp_sprintf("%s/%s", test_files[i], "out"));
+        nob_temp_sprintf("tests/%s/%s", *test_file, "comp-stdout.txt"));
+    nob_delete_file(nob_temp_sprintf("tests/%s/%s", *test_file, "out"));
 
     nob_temp_rewind(mark);
   }
 
   double elapsed_ms = ms_since(t0);
+
+  int passed = 0;
+  int failed = 0;
 
   nob_da_foreach(TestResult, it, &assertions) {
     log_test_result(*it);
